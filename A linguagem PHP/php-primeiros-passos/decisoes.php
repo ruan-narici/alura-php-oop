897f43b7665188bc0f550ff1956a0f563d135a52
Ruan Narici <ruannarici@hotmail.com>
2023-06-28 18:49:27 -0300

    A linguagem PHP

---
+++ A linguagem PHP/php-primeiros-passos/decisoes.php
@@ -0,0 +1,12 @@
+<?php
+
+$idade = 24;
+
+if($idade >= 18) {
+    echo "Você tem $idade anos." . PHP_EOL;
+    echo "Pode entrar.";
+} else {
+    echo "Você só tem $idade. Não pode entrar.";
+}
+
+?>
