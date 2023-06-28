0fbbec64a5053d77170a61045bee4a5ee471303c
Ruan Narici <ruannarici@hotmail.com>
2023-06-28 20:27:30 -0300

    A linguagem PHP

---
+++ A linguagem PHP/php-primeiros-passos/desafios/tabuada.php
@@ -0,0 +1,11 @@
+<?php
+
+for ($i = 0; $i <= 10; $i++) {
+    for ($n = 0; $n <= 10; $n++) {
+        $multiplicacao = $i * $n;
+        echo "$i x $n = $multiplicacao." . PHP_EOL;
+    }
+    echo PHP_EOL;
+}
+
+?>
