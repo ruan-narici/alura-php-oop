c1a075d2dcf2b56db75088cfe473fd6ae00a6249
Ruan Narici <ruannarici@hotmail.com>
2023-06-30 22:53:19 -0300

    A linguagem PHP

--- A linguagem PHP/php-primeiros-passos/avancando/banco.php
+++ A linguagem PHP/php-primeiros-passos/avancando/banco.php
@@ -1,14 +1,23 @@
 <?php
 
-function exibeMensagem($mensagem) {
+function exibeMensagem(string $mensagem): void {
     echo $mensagem . PHP_EOL;
 }
 
-function sacar($conta, $valor) {
+function sacar(array $conta, float $valor): array {
     if ($valor <= $conta['saldo']) {
         $conta['saldo'] -= $valor;
     } else {
         echo $conta['titular'] . ", você não possui saldo suficiente." . PHP_EOL;
+    }
+    return $conta;
+}
+
+function depositar(array $conta, float $valor): array {
+    if ($valor > 0) {
+        $conta['saldo'] += $valor;
+    } else {
+        echo "Valor inválido!";
     }
     return $conta;
 }
@@ -31,10 +40,13 @@ $contasCorrentes = [
     ]
 ];
 
-$contasCorrentes['12320219232'] = sacar($contasCorrentes['12320219232'], 1500);
+$contasCorrentes['12320219232'] = sacar($contasCorrentes['12320219232'], 500);
+
+$contasCorrentes['34920193291'] = depositar($contasCorrentes['34920193291'], 500);
 
 foreach ($contasCorrentes as $cpf => $conta) {
     exibeMensagem($cpf . " - " . $conta['titular'] . " " . $conta['saldo']);
 }
+
 
 ?>
