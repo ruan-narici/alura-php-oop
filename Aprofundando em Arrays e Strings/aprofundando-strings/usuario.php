d0fd7d7257761445091ecb7be73335dfae33f869
Ruan Narici <ruannarici@hotmail.com>
2023-07-11 19:15:00 -0300

    Aprofundando em Strings e Arrays

--- Aprofundando em Arrays e Strings/aprofundando-strings/usuario.php
+++ Aprofundando em Arrays e Strings/aprofundando-strings/usuario.php
@@ -5,4 +5,12 @@ $email = 'ruan123@gmail.com';
 echo substr($email, 0, 7) . PHP_EOL;
 echo substr($email, 7) . PHP_EOL;
 
+$email2 = 'ruan777@gmail.com';
+
+// Capturando o indice do arroba
+$indiceArroba = strpos($email2, '@', 0);
+
+// Exibindo o que tem depois do @ 
+echo substr($email2, $indiceArroba +1);
+
 ?>
