849dc0f0a326424335df1a1720cddf2ee326f963
Ruan Narici <ruannarici@hotmail.com>
2023-07-11 16:08:33 -0300

    Aprofundando em Strings e Arrays

---
+++ Aprofundando em Arrays e Strings/aprofundando-strings/url.php
@@ -0,0 +1,12 @@
+<?php
+
+$url = 'https://www.google.com';
+
+// Verifica se uma String comeca com uma determinada sequencia
+var_dump(str_starts_with($url, 'https'));
+
+// Verifica se uma String termina com uma determinada sequencia
+var_dump(str_ends_with($url, '.br'));
+
+
+?>
