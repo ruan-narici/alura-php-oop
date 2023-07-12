b10567b61e9cd91df6234a524ee9adfb7456e3e7
Ruan Narici <ruannarici@hotmail.com>
2023-07-12 19:33:21 -0300

    Aprofundando em Strings e Arrays

---
+++ Aprofundando em Arrays e Strings/aprofundando-strings/telefoneImplode.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$telefones = ["(77) 99999 - 9999", "(71) 99999 - 9999", "(74) 99999 - 9999"];
+
+// Com o implode é possível fazer a exibicao de um array definindo tambem 
+// qual sera o separador utilizado
+echo implode(PHP_EOL, $telefones);
+
+?>
