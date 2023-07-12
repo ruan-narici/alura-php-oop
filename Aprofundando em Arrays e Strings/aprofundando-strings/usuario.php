db2520cab7c6691c409ff59d2ba03891d722e10b
Ruan Narici <ruannarici@hotmail.com>
2023-07-12 19:19:08 -0300

    Aprofundando em Strings e Arrays

--- Aprofundando em Arrays e Strings/aprofundando-strings/usuario.php
+++ Aprofundando em Arrays e Strings/aprofundando-strings/usuario.php
@@ -11,7 +11,15 @@ $email2 = 'ruan777@gmail.com';
 $indiceArroba = strpos($email2, '@', 0);
 
 // Exibindo o que tem depois do @ 
-echo substr($email2, $indiceArroba +1);
+echo substr($email2, $indiceArroba +1) . PHP_EOL;
 
+
+$pessoa = 'Ruan Narici Mendonça';
+// Fazendo com que a variavel $pessoa seja partida em varios pedaços, separados
+// por espaço e atribuindo esses valores a outras variaveis atraves do list();
+
+list($primeiroNome, $segundoNome, $terceiroNome) = explode(' ', $pessoa );
+
+echo "Nome: $primeiroNome; Sobrenome: $segundoNome $terceiroNome;" . PHP_EOL;
 
 ?>
