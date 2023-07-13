8c20b302f413106665b12949084b92fc87f2c3b3
Ruan Narici <ruannarici@hotmail.com>
2023-07-12 21:41:45 -0300

    Aprofundando em Strings e Arrays

---
+++ Aprofundando em Arrays e Strings/aprofundando-strings/removendoEspaco.php
@@ -0,0 +1,16 @@
+<?php
+
+$nomeComEspaco = "   Ruan Narici Mendonça   ";
+
+// Remove o espaço antes e depois da nossa String
+echo trim($nomeComEspaco) . PHP_EOL;
+
+
+// Remove o espaço antes da nossa String
+echo ltrim($nomeComEspaco) . PHP_EOL;
+
+// Remove o espaço depois da nossa String
+echo rtrim($nomeComEspaco) . PHP_EOL;
+
+
+?>
