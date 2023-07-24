97e0f829994ec630fa134fb50327d2f03a290425
Ruan Narici <ruannarici@hotmail.com>
2023-07-24 19:07:50 -0300

    Orientação a Objetos

--- Orientação a Objetos/Classes, metodos e atributos/Conta.php
+++ Orientação a Objetos/Classes, metodos e atributos/Conta.php
@@ -4,6 +4,34 @@ class Conta {
     public String $nome;
     public String $cpf;
     public float $saldo;
+
+    public function sacar(float $valor): void {
+        if ($valor > $this->saldo) {
+            echo "Saldo insuficiente!" . PHP_EOL;
+            return;
+        }
+
+        $this->saldo -= $valor;
+    }
+
+    public function depositar(float $valor): void {
+        if ($valor < 0) {
+            echo "O valor deve ser maior que 0." . PHP_EOL;
+            return;
+        }
+
+        $this->saldo += $valor;
+    }
+
+    public function transferir(float $valor, Conta $conta): void {
+        if ($valor < 0 || $valor > $this->saldo) {
+            echo "O valor deve ser maior que 0 ou superior ao seu saldo." . PHP_EOL;
+            return;
+        }
+
+        $this->saldo -= $valor;
+        $conta->depositar($valor);
+    }
 }
 
 $primeiraConta = new Conta();
@@ -11,6 +39,16 @@ $primeiraConta->nome = 'Ruan Narici';
 $primeiraConta->cpf = '000.000.000-00';
 $primeiraConta->saldo = 10000;
 
+$segundaConta = new Conta();
+$segundaConta->nome = 'Cinthia Oliveira';
+$segundaConta->cpf = '111.111.111-11';
+$segundaConta->saldo = 10000;
+
+$primeiraConta->transferir(2000, $segundaConta);
+$segundaConta->sacar(2000);
+
+
 var_dump($primeiraConta);
+var_dump($segundaConta);
 
 ?>
