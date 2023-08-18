06f42624d97097231f11484ea6b803ac8d247c4a
Ruan Narici <ruannarici@hotmail.com>
2023-08-17 21:43:52 -0300

    Manipulação de arquivos e tratamento de erros

--- Manipulação de arquivos e tratamento de erros/projeto/consoleParaArquivos.php
+++ Manipulação de arquivos e tratamento de erros/projeto/consoleParaArquivos.php
@@ -1,7 +1,16 @@
 <?php
 
-$novoCurso = trim(fgets(STDIN));
+//$novoCurso = trim(fgets(STDIN));
 
-file_put_contents(__DIR__ . '/cursosPhp.txt', "\n$novoCurso", FILE_APPEND);
+// file_put_contents(__DIR__ . '/cursosPhp.txt', "\n$novoCurso", FILE_APPEND);
+
+// Abrindo arquivo no modo leitura
+$arquivo = fopen(__DIR__ . '/cursosPhp.txt', 'r');
+
+// Fazendo uma especie de echo com o fwrite e o STDOUT
+fwrite(STDOUT, "LISTA DE CURSOS: \n");
+
+// Copiando os stream do arquivo e jogando no console atraves do STDOUT
+stream_copy_to_stream($arquivo, STDOUT);
 
 ?>
