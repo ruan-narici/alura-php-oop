5197a90e7653d50c0b3d10c077591eb676adf62a
Ruan Narici <ruannarici@hotmail.com>
2023-08-19 19:37:15 -0300

    Manipulação de arquivos e tratamento de erros

---
+++ Manipulação de arquivos e tratamento de erros/projeto/spl.php
@@ -0,0 +1,16 @@
+<?php
+
+$arquivo = new SplFileObject(__DIR__ . "/arquivoCsv.csv", "r");
+
+while (!$arquivo->eof()) {
+    $linha = $arquivo->fgetcsv(";");
+    echo $linha[0] . PHP_EOL;
+}
+
+$date = new DateTime();
+
+$date->setTimestamp($arquivo->getCTime());
+
+echo "DATA DE CRIACAO: " . $date->format("d/m/Y") . PHP_EOL;
+
+?>
