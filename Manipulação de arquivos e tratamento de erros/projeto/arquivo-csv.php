7d93e6bbde45547ea795dbacba8c8b3031569757
Ruan Narici <ruannarici@hotmail.com>
2023-08-19 18:53:24 -0300

    Manipulação de arquivos e tratamento de erros

---
+++ Manipulação de arquivos e tratamento de erros/projeto/arquivo-csv.php
@@ -0,0 +1,25 @@
+<?php
+
+$meusCursos = file(__DIR__ . "/listaCursos.txt");
+$outrosCursos = file(__DIR__ . "/escritorCursos.txt");
+
+$arquivo = fopen(__DIR__ . "/arquivoCsv.csv", "w");
+fputcsv($arquivo, ["Nome do Curso", "Aula feita por mim"], ";");
+
+foreach($meusCursos as $curso) {
+    $linha = [trim($curso), "sim"];
+
+    fputcsv($arquivo, $linha, ";");
+};
+
+foreach($outrosCursos as $curso) {
+    $linha = [trim($curso), "nao"];
+
+    fputcsv($arquivo, $linha, ";");
+};
+
+fclose($arquivo);
+
+echo file_get_contents(__DIR__ . "/arquivoCsv.csv");
+
+?>
